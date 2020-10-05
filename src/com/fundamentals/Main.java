cbd1518272c968d973e6c5c687ffd6f4bd6fb01a
ABHI <siridoro1090@gmail.com>
2020-10-05 20:26:30 +1100

    modified system out statement 13

--- src/com/fundamentals/Main.java
+++ src/com/fundamentals/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
 		System.out.println(impl.countUniqueKeys(null));
 
-		System.out.println(impl.searchCount(null, "software"));
+		System.out.println(impl.searchCount(null, "soft"));
 	}
 
 }
